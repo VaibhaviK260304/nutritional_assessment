8006e48a37692b6e8750e944f9d1760d47cef503
AditiNaldurgkar <aditinaldurgkar03@gmail.com>
2025-02-21 07:36:04 +0530

    Patient model created

---
+++ server/models/Patient.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import  {model,Schema} from  mongoose;
+const PatientSchema = new Schema({
+name : String ,
+age :Number,
+gender : String,
+height:Number,
+weight:Number,
+});
+
+const Patient= model('Patient',PatientSchema);
+export default Patient;
